16951927feb93ac02038a1527b416cd8bd194a6d
yuvalman <yuvalman958@gmail.com>
2022-06-05 08:53:32 +0300

    step2: create api for s3bucket crd (#3)
    create api for s3bucket crd using the command:
    `kubebuilder create api --group aws --version v1 --kind S3Bucket`
    
    Note: You will be asked for creating Resource and Controller, enter "y" for both of the questions

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	awsv1 "github.com/yuvalman/s3BucketController/api/v1"
+	"github.com/yuvalman/s3BucketController/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(awsv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.S3BucketReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "S3Bucket")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
